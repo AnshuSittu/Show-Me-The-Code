ac5bc6ea6c7030b9f1f4693ba418f9ee140e5ade
Anshu <anshu.possible@gmail.com>
2024-02-19 18:00:11 +0530

    added Inline css by creating function

--- App.js
+++ App.js
@@ -2,29 +2,65 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 
-const elem = <span>This is React Element</span>
 
-const Title = () => (
-  <h1 className="head">
 
-       Namste React using JSX 🚀
-  </h1>
-  
-);
-const number = 5000;
 
-const HeadingComponent = () => (
-  <div id="container">
-    <Title />
-    {Title()}
-    <Title></Title>
-    <h2>{number}</h2>
-    <h1>Namste React Function Component</h1>
-  </div>
-);
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img
+          className="logo"
+          src="https://png.pngtree.com/png-vector/20220708/ourmid/pngtree-fast-food-logo-png-image_5763171.png"
+        />
+      </div>
 
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+
+const styleCard = {
+  backgroundColor:"#D4E7C5"
+}
+
+
+const RestaurantCard = () => {
+  return (
+    <div className="res-card" style={styleCard}>
+      <h3>Spaices</h3>
+    </div>
+  );
+};
+
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search">Search </div>
+      <div className="res-conatiener">
+        <RestaurantCard />
+      </div>
+    </div>
+  );
+};
+
+const AppLayout = () => {
+  return (
+    <div className="App">
+      <Header />
+      <Body/>
+    </div>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<HeadingComponent />);
-
+root.render(<AppLayout />);
